8a0bf8be887a82c5aeb1a4cc43836afbe519b0ad
Dzaki Wiradinata <dzaki.wiradinata@gmail.com>
2022-03-31 19:53:40 -0400

    Added function game() that runs through rounds until player or computer gets to 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 // Randomly picks between ROCK, PAPER, or SCISSORS
 function computerPlay() {
     let pick = ['ROCK', 'PAPER', 'SCISSORS']
@@ -7,45 +8,84 @@ function computerPlay() {
 // Plays a round and returns the winner
 function playRound(playerSelection, computerSelection) {
 
-    let outcome = '';
     playerSelection = playerSelection.toUpperCase();
+    // computerSelection = computerPlay();
 
-    console.log(playerSelection)
-    console.log(computerSelection)
+    console.log(playerSelection);
+    console.log(computerSelection);
 
     if(computerSelection === 'ROCK' && playerSelection != 'ROCK') {
         if(playerSelection === 'PAPER') {
             outcome = 'You Win! PAPER beats ROCK.';
+            return true;
         }
         else {
             outcome = 'You Lose! ROCK beats SCISSORS.';
+            return false;
         }
-        return outcome;
     }
+
     else if(computerSelection === 'PAPER' && playerSelection != 'PAPER') {
         if(playerSelection === 'SCISSORS') {
             outcome = 'You Win! SCISSORS beats PAPER.';
+            return true;
         }
         else {
             outcome = 'You Lose! PAPER beats ROCK.';
+            return false;
         }
-        return outcome;
     }
+
     else if(computerSelection === 'SCISSORS' && playerSelection != 'SCISSORS') {
         if(playerSelection === 'ROCK') {
             outcome = 'You Win! ROCK beats SCISSORS.';
+            return true;
         }
         else {
             outcome = 'You Lose! SCISSORS beats PAPER.';
+            return false;
         }
-        return outcome;
     }
+
     else {
-        return outcome = "It's a draw!"
+        return null;
     }
 }
 
-const playerSelection = "PaPeR";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game(){
 
+    let playerScore = 0;
+    let computerScore = 0;
+    // let playerSelection = prompt("Type ond of Rock, Paper, Scissors");
+    // const computerSelection = computerPlay()
+
+    for (let playerScore = 0, computerScore = 0; computerScore < 5 && playerScore <5;) {
+        let playerSelection = prompt("Type ond of Rock, Paper, Scissors");
+        const computerSelection = computerPlay();
+
+        if (playRound(playerSelection, computerSelection)) {
+            playerScore++;
+        }
+        else if(playRound(playerSelection, computerSelection) === false){
+            computerScore++;
+        }
+        else {
+            console.log("It's a draw!");
+        }
+
+    console.log(playerScore)
+    console.log(computerScore)
+    }
+    console.log(playerScore)
+    console.log(computerScore)
+
+
+
+
+    // console.log(playRound(playerSelection, computerSelection));
+    // console.log(playerScore)
+    // console.log(computerScore)
+}
+
+
+game();
